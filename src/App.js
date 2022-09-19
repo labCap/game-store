5b057e915fbd568f6cd75d54f1ee12585a7248e3
LabCap <banlazthosow@gmail.com>
2022-09-19 22:16:34 +0300

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import { Header } from "./components/header";
 import { HomePage } from "./pages/home-page/";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
         </Routes>
