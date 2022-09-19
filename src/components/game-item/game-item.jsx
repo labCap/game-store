e65ad693b9bee47d2d6e1e6dbf4b8d2eba9438ce
LabCap <banlazthosow@gmail.com>
2022-09-19 23:14:44 +0300

    updated game-item.jsx

--- src/components/game-item/game-item.jsx
+++ src/components/game-item/game-item.jsx
@@ -1,17 +1,23 @@
 import React from "react";
+import { GameBuy } from "../game-buy";
 import { GameCover } from "../game-cover";
+import { GameGenre } from "../game-genre";
 import "./game-item.css";
 
 export const GameItem = ({ game }) => {
   return (
     <div className="game-item">
-      <GameCover />
+      <GameCover image={game.image} />
       <div className="game-item__details">
         <span className="game-item__title">{game.title}</span>
         <div className="game-item__genre">
-          {game.genres.map((genre) => genre)}
+          {game.genres.map((genre) => (
+            <GameGenre genre={genre} key={genre} />
+          ))}
         </div>
-        <div className="game-item__buy">Buy</div>
+        <div className="game-item__buy">
+          <GameBuy game={game} />
+        </div>
       </div>
     </div>
   );
