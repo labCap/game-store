e6fc876e91890824bc804176d0f67bf11a8b6c8f
LabCap <banlazthosow@gmail.com>
2022-09-19 23:12:19 +0300

    updated game-cover

--- src/components/game-cover/game-cover.jsx
+++ src/components/game-cover/game-cover.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import "./game-cover.css";
 
-export const GameCover = () => {
-  return <div className="game-cover"></div>;
+export const GameCover = ({ image = "" }) => {
+  return (
+    <div className="game-cover" style={{ backgroundImage: `url(${image})` }} />
+  );
 };
