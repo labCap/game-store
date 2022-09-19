346b54ffb0a2730a6346df1d0009e6b38b6697d3
LabCap <banlazthosow@gmail.com>
2022-09-19 21:33:33 +0300

    home-page/ created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import { HomePage } from "./pages/home-page/";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/home-page/home-page.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./home-page.css";
+
+export const HomePage = () => {
+  return <div className="home-page">HomePage</div>;
+};
