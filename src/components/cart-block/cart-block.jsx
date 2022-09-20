79cc72b5981234ffe1ab51999e7e1ede6db08c41
LabCap <banlazthosow@gmail.com>
2022-09-20 14:41:21 +0300

    added the ability to open/close the cart

--- src/components/cart-block/cart-block.jsx
+++ src/components/cart-block/cart-block.jsx
@@ -1,19 +1,26 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState } from "react";
 import { BiCartAlt } from "react-icons/bi";
+import { useSelector } from "react-redux";
 import { CartMenu } from "../cart-menu";
-import "./cart-block.css";
 import { calcTotalPrice } from "../utils";
+import "./cart-block.css";
 
 export const CartBlock = () => {
+  const [isCartMenuVisible, setCartMenuVisible] = useState(false);
   const items = useSelector((state) => state.cart.itemsInCart);
   const totalPrice = calcTotalPrice(items);
 
   return (
     <div className="cart-block">
-      <BiCartAlt size={25} className="cart-block__icon" />
-      <span className="cart-block__total-price">{totalPrice} uan</span>
-      <CartMenu items={items} onClick={() => null} />
+      <BiCartAlt
+        size={25}
+        className="cart-block__icon"
+        onClick={() => setCartMenuVisible(!isCartMenuVisible)}
+      />
+      {totalPrice > 0 ? (
+        <span className="cart-block__total-price">{totalPrice} uan</span>
+      ) : null}
+      {isCartMenuVisible && <CartMenu items={items} onClick={() => null} />}
     </div>
   );
 };
