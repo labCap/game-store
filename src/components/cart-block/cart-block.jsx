8169c9c681c3066dd8a938893a1e118c1473a59b
LabCap <banlazthosow@gmail.com>
2022-09-20 13:57:06 +0300

    added logic for adding prices to the cart

--- src/components/cart-block/cart-block.jsx
+++ src/components/cart-block/cart-block.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { BiCartAlt } from "react-icons/bi";
 import "./cart-block.css";
 
 export const CartBlock = () => {
+  const items = useSelector((state) => state.cart.itemsInCart);
+  const totalPrice = items.reduce((acc, game) => (acc += game.price), 0);
+
   return (
     <div className="cart-block">
       <BiCartAlt size={25} className="cart-block__icon" />
-      <span className="cart-block__total-price">1000 uan</span>
+      <span className="cart-block__total-price">{totalPrice} uan</span>
     </div>
   );
 };
