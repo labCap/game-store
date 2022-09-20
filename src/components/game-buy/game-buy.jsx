fe8d292f19baf8f5a1eca4933ac8397f03452510
LabCap <banlazthosow@gmail.com>
2022-09-20 14:35:38 +0300

    added product removal from cart

--- src/components/game-buy/game-buy.jsx
+++ src/components/game-buy/game-buy.jsx
@@ -1,21 +1,32 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import { setItemInCart } from "../../redux/cart/reducer";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteItemInCart, setItemInCart } from "../../redux/cart/reducer";
 import { Button } from "../button";
 import "./game-buy.css";
 
 export const GameBuy = ({ game }) => {
   const dispatch = useDispatch();
+  const items = useSelector((state) => state.cart.itemsInCart);
+  const isItemInCart = items.some((item) => item.id == game.id);
+
   const handleClick = (e) => {
     e.stopPropagation();
-    dispatch(setItemInCart(game));
+
+    if (isItemInCart) {
+      dispatch(deleteItemInCart(game.id));
+    } else {
+      dispatch(setItemInCart(game));
+    }
   };
 
   return (
     <div className="game-buy">
       <span className="game-buy__price">{game.price} uan</span>
-      <Button type={"primary"} onClick={handleClick}>
-        В корзину
+      <Button
+        type={isItemInCart ? "secondary" : "primary"}
+        onClick={handleClick}
+      >
+        {isItemInCart ? "Убрать из корзины" : "В корзину"}
       </Button>
     </div>
   );
