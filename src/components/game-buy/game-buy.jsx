0c0c7736bdd3ca4192dbc49bf84598016748c664
LabCap <banlazthosow@gmail.com>
2022-09-20 13:47:47 +0300

    Added logic add item to cart

--- src/components/game-buy/game-buy.jsx
+++ src/components/game-buy/game-buy.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { setItemInCart } from "../../redux/cart/reducer";
 import { Button } from "../button";
 import "./game-buy.css";
 
 export const GameBuy = ({ game }) => {
+  const dispatch = useDispatch();
+  const handleClick = (e) => {
+    e.stopPropagation();
+    dispatch(setItemInCart(game));
+  };
+
   return (
     <div className="game-buy">
       <span className="game-buy__price">{game.price} uan</span>
-      <Button type={"primary"} onClick={() => null}>
+      <Button type={"primary"} onClick={handleClick}>
         В корзину
       </Button>
     </div>
