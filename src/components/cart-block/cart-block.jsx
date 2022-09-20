a8dd00947cb263c90176a79f399b71f12f72f089
LabCap <banlazthosow@gmail.com>
2022-09-20 14:23:24 +0300

    Added style add item to cart

--- src/components/cart-block/cart-block.jsx
+++ src/components/cart-block/cart-block.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { BiCartAlt } from "react-icons/bi";
+import { CartMenu } from "../cart-menu";
 import "./cart-block.css";
+import { calcTotalPrice } from "../utils";
 
 export const CartBlock = () => {
   const items = useSelector((state) => state.cart.itemsInCart);
-  const totalPrice = items.reduce((acc, game) => (acc += game.price), 0);
+  const totalPrice = calcTotalPrice(items);
 
   return (
     <div className="cart-block">
       <BiCartAlt size={25} className="cart-block__icon" />
       <span className="cart-block__total-price">{totalPrice} uan</span>
+      <CartMenu items={items} onClick={() => null} />
     </div>
   );
 };
